533fd2905361f9f17859520c770a5938813e4d2a
ninjaboynaru <thiagoCord755@gmail.com>
2022-01-20 22:22:52 -0500

    Post date UI is relative

--- apps/client/js/PostsScreen/Post.js
+++ apps/client/js/PostsScreen/Post.js
@@ -2,12 +2,26 @@ import React from 'react';
 import propTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import * as dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import Button from '../Button';
 
+dayjs.extend(relativeTime);
+
 function Post({ post, likePost, dislikePost }) {
+	const parsedDate = dayjs(post.date);
+	let dateText;
+
+	if (parsedDate.isValid() === false) {
+		dateText = 'Unknown Date';
+	}
+	else {
+		dateText = parsedDate.fromNow();
+	}
+
 	return (
 		<div className="post">
-			<span className="post__date">{post.date}</span>
+			<span className="post__date">{dateText}</span>
 			<span className="post__text">{post.text}</span>
 			<div className="post__control-bar">
 				<Button primary outline onClick={likePost}><FontAwesomeIcon className="posts__control-bar__icon" icon={faThumbsUp} />{post.likes}</Button>
